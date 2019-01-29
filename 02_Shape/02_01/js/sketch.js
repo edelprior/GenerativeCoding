0618027ee828f9e1923bfa9f3ea0e19d51ec8216
edelprior <edelprior@hotmail.com>
2019-01-29 13:22:57 +0000

    d

--- 02_Shape/02_01/js/sketch.js
+++ 02_Shape/02_01/js/sketch.js
@@ -2,23 +2,22 @@
 // Notes
 // Manipulating the Co-Oridnate Systems
 
-let radius = 150;
+var radius = 150;
 let angle = 45;
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
-  background(255, 245,245);
-
+  background(255, 245, 245);
 }
 
 function draw() {
   let posX = cos(angle) * radius;
   let posY = sin(angle) * radius;
 
-  translate(width/2, height/2);
-  stroke(0,0,125, 85);
+  translate(width / 2, height / 2);
+  stroke(0, 0, 125, 85);
   strokeWeight(10);
   line(0, 0, posX, posY);
-  angle ++;
+  angle++;
 }
